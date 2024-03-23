792a43a7b96af6ddf38336bfeb92b7e7b020611e
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-03-23 14:42:48 +0300

    Реализовано распознавание лица прямо в браузере по адресу: http://127.0.0.1:8000/video_stream

--- main.py
+++ main.py
@@ -1,22 +1,65 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import face_recognition
+import cv2
+import numpy as np
+from starlette.responses import StreamingResponse
 
 app = FastAPI()
-
-# Подключаем статические файлы (HTML, CSS, JS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Подключаем шаблоны Jinja2
 templates = Jinja2Templates(directory="templates")
 
+video_capture = cv2.VideoCapture(0)
+person_image = face_recognition.load_image_file("students/person.jpg")
+person_face_encoding = face_recognition.face_encodings(person_image)[0]
+known_face_encodings = [person_face_encoding]
+known_face_names = ["Alexander Poltorapavlov"]
 
 @app.get("/")
 async def get_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+def generate_frames():
+    while True:
+        ret, frame = video_capture.read()
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+            face_names.append(name)
+
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame_bytes = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
+
+@app.get("/video_feed")
+async def video_feed():
+    return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+@app.get("/video_stream")
+async def video_stream(request: Request):
+    return templates.TemplateResponse("video_stream.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="127.0.0.1", port=8000)
